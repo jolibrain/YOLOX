2c015a6aa9d6fddf0fcd1657223ce135e6cc0a66
Louis Jean <louis.jean@jolibrain.com>
2024-03-18 14:42:15 +0100

    fix: torch_no_grad not working with torch.jit.script
    see https://github.com/pytorch/pytorch/issues/97876

--- yolox/models/yolo_head.py
+++ yolox/models/yolo_head.py
@@ -315,28 +315,29 @@ class YOLOXHead(nn.Module):
                 gt_classes = labels[batch_idx, :num_gt, 0]
                 bboxes_preds_per_image = bbox_preds[batch_idx]
 
-                (
-                    gt_matched_classes,
-                    fg_mask,
-                    pred_ious_this_matching,
-                    matched_gt_inds,
-                    num_fg_img,
-                ) = self.get_assignments(  # noqa
-                    batch_idx,
-                    num_gt,
-                    total_num_anchors,
-                    gt_bboxes_per_image,
-                    gt_classes,
-                    bboxes_preds_per_image,
-                    expanded_strides,
-                    x_shifts,
-                    y_shifts,
-                    cls_preds,
-                    bbox_preds,
-                    obj_preds,
-                    labels,
-                    imgs,
-                )
+                with torch.no_grad():
+                    (
+                        gt_matched_classes,
+                        fg_mask,
+                        pred_ious_this_matching,
+                        matched_gt_inds,
+                        num_fg_img,
+                    ) = self.get_assignments(  # noqa
+                        batch_idx,
+                        num_gt,
+                        total_num_anchors,
+                        gt_bboxes_per_image,
+                        gt_classes,
+                        bboxes_preds_per_image,
+                        expanded_strides,
+                        x_shifts,
+                        y_shifts,
+                        cls_preds,
+                        bbox_preds,
+                        obj_preds,
+                        labels,
+                        imgs,
+                    )
                 """
                 try:
                     (
@@ -467,7 +468,6 @@ class YOLOXHead(nn.Module):
         l1_target[:, 3] = torch.log(gt[:, 3] / stride + eps)
         return l1_target
 
-    @torch.no_grad()
     def get_assignments(
         self,
         batch_idx: int,
